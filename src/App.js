9b0241a2efd2de342af4185c02bb9a34795fbc51
ZuluMumu <saugatdahal030@gmail.com>
2024-08-04 23:03:11 +0545

    LearnedPropsinReact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="bg-black text-white">Hello</h1>
-    </div>
+    <>
+      <Navbar title="ReactSikdai" contact="contact" />
+      {/* Yo chai props.title vanne variable ma gako jastai samjhinu. title ma je value rakhyo tyo value gayera props object ma janxa ani tya component load huda replace hunxa. Contact chai gayab hunxa aba since i passed  null value*/}
+      <h1 className="text-slate-800 font-bold text-5xl">Hello World</h1>
+    </>
   );
 }
 
-export default App;
+export default App; //Yo logic chai need to revise once again. EXport Import wala.

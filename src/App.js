f007660100527628778780451556eb6d08891a93
ZuluMumu <saugatdahal030@gmail.com>
2024-08-04 23:48:02 +0545

    BasicsOFstate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Myform from "./components/Myform"; //component ko naam always capital.
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,9 +7,11 @@ function App() {
     <>
       <Navbar title="ReactSikdai" contact="contact" />
       {/* Yo chai props.title vanne variable ma gako jastai samjhinu. title ma je value rakhyo tyo value gayera props object ma janxa ani tya component load huda replace hunxa. Contact chai gayab hunxa aba since i passed  null value*/}
-      <h1 className="text-slate-800 font-bold text-5xl">Hello World</h1>
+      {/* <h1 className="text-slate-800 font-bold text-5xl">Hello World</h1> */}
+      <Myform place="Enter your text" butName="Change Case" />
+      {/* place and butName are passed it as a prop in the Myform component */}
     </>
   );
 }
 
-export default App; //Yo logic chai need to revise once again. EXport Import wala.
+export default App; //Yo logic chai need to revise once again. Export Import wala.
---
+++ src/components/Myform.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react"; //useState hook ho euta jun chia class based component ma thiyo function based ma use garna yo garnai parxa teti ho
+
+export default function Myform(props) {
+  const handleClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter your text");
+  //   text = "fdsfsdf"; not allowed
+  // setText = "jsjsjsj"; allowed
+  return (
+    <>
+      <form className="w-full mx-auto  max-w-sm my-24">
+        <h1 className="text-xl text-slate-700">{props.place} :</h1>
+        {/* state variable use gareko this constant variable cannot be changed unless setText is used. */}
+
+        <div className="flex items-center border-b border-teal-500 py-2">
+          <input
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            id="myInput"
+            value={text}
+            placeholder={props.place}
+            onChange={handleChange}
+          />
+          {/* aba onChange use garera balla edit garna milxa tyo constant value={text} lai  */}
+          {/* since I have used it in the value feild of the input tag , the value cannpt be changed from {text} as it is a constant. */}
+          {/* however it can be changed by using useState when firing an event.           */}
+
+          <button
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+            onClick={handleClick}
+          >
+            {props.butName}
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
